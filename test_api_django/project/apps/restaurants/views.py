380bf9d0a3adec8f0b0745e2ceb7c1b9b27fa7e9
Julio Marins <julio.marins@esharesinc.com>
2018-03-03 10:40:33 -0300

    register with django default UserCreationForm

--- test_api_django/project/apps/restaurants/views.py
+++ test_api_django/project/apps/restaurants/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from project.apps.restaurants.models import Restaurant
 from project.apps.restaurants.serializers import RestaurantSerializer
@@ -9,3 +13,17 @@ class RestaurantViewSet(viewsets.ModelViewSet):
     """
     serializer_class = RestaurantSerializer
     queryset = Restaurant.objects.all()
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
--- test_api_django/project/urls.py
+++ test_api_django/project/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
+from project.apps.restaurants import views
 
 admin.autodiscover()
 
 
 urlpatterns = (
     url(r'^$', RedirectView.as_view(url='/api')),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
