9451ca2ef5471eb7d687592be9528e9c20e74ac4
Julio Marins <julio.marins@esharesinc.com>
2018-03-04 12:10:22 -0300

    fixing css on profile page and displaying correct topic name

--- test_api_django/project/apps/restaurants/forms.py
+++ test_api_django/project/apps/restaurants/forms.py
@@ -4,6 +4,11 @@ from project.apps.restaurants.models import Profile
 class ProfileForm(forms.ModelForm):
     read_only = ('uuid',)
 
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'u-full-width'
+
     class Meta:
         model = Profile
         fields = ('first_name', 'last_name', 'current_position', 'about_you', 'topics')
--- test_api_django/project/apps/restaurants/models.py
+++ test_api_django/project/apps/restaurants/models.py
@@ -14,6 +14,9 @@ class Restaurant(models.Model):
 class Topic(models.Model):
     name = models.CharField(max_length=12, blank=True)
 
+    def __str__(self):
+        return u"%s" % self.name
+
 
 class Profile(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
