260b6a9c6ea6dca2e3c846fff3ce315b27895d2f
Julio Marins <julio.marins@esharesinc.com>
2018-03-09 18:58:50 -0300

    improving profile page

--- test_api_django/project/apps/mini_project/views.py
+++ test_api_django/project/apps/mini_project/views.py
@@ -36,6 +36,7 @@ class ProfileDetail(TemplateView):
             profile_form = ProfileForm()
 
         return {
+            'profile': profile,
             'is_user_page': profile.user == self.request.user,
             'profile_form': profile_form,
             'topics': self.request.user.profile.get_topics_ordered(profile),
