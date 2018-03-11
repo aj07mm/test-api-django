1aeb06c978aa1e5e9a1b46bda61a79246d144455
Julio Marins <julio.marins@esharesinc.com>
2018-03-11 14:26:12 -0300

    urls fixed by view inheritance

--- project/apps/motius/api/views.py
+++ project/apps/motius/api/views.py
@@ -1,12 +1,16 @@
 from rest_framework import viewsets, mixins
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from project.apps.motius.api.serializers import ArticleSerializer
 from project.apps.motius.models import Article
 from project.pagination import BasePaginator
 
 
-class ArticleViewSet(viewsets.ViewSet):
-    #permission_classes = (IsAuthenticated,)
+class ArticleViewSet(viewsets.ModelViewSet):
+    permission_classes = (
+        IsAuthenticated,
+    )
 
     serializer_class = ArticleSerializer
     pagination_class = BasePaginator
