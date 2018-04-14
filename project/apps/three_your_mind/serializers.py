5447112e8cd40c7538c9e060307747733bb084a9
aj07mm <aj07mm@hotmail.com>
2018-04-14 16:20:43 -0300

    working with nested but still adding extra fields

--- project/apps/three_your_mind/serializers.py
+++ project/apps/three_your_mind/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from django.db import models
 from project.apps.three_your_mind.models import Printer
-from collections import OrderedDict
+from collections import OrderedDict, Iterable
 from rest_framework.relations import Hyperlink, PKOnlyObject  # NOQA # isort:skip
 
 
 class NestingSerializer(serializers.Serializer):
 
-    def to_representation(self, instance):
+    def get_to_representation(self, instance):
         """
         Object instance -> Dict of primitive datatypes.
         """
@@ -32,6 +32,32 @@ class NestingSerializer(serializers.Serializer):
                 ret[field.field_name] = field.to_representation(attribute)
 
         return ret
+
+    def replace_values(self, obj, current_value_sign, next_value):
+        new_obj = OrderedDict()
+            
+        if isinstance(obj, dict):
+            for key, value in obj.items():
+                new_obj[key] = self.replace_values(value, current_value_sign, next_value)
+            return new_obj
+
+        if obj == current_value_sign:
+            return next_value
+        return obj
+
+    def to_representation(self, instance):
+        ret = self.get_to_representation(instance)
+        ret = { **ret, **self.Meta.nesting }
+
+        fields = self._readable_fields
+        # replace fields
+        for field in fields:
+            ret = self.replace_values(ret, field.field_name, field.get_attribute(instance))
+        # replace nesting
+        for key, value in self.Meta.nesting.items():
+            ret = self.replace_values(ret, key, value)
+
+        return ret
     
 
 class PrinterSerializer(NestingSerializer, serializers.ModelSerializer):
@@ -47,6 +73,8 @@ class PrinterSerializer(NestingSerializer, serializers.ModelSerializer):
         nesting = {
             "productionTime": {
                 "minimum" : "min_production_time",
-                "maximum" : "max_production_time"
-            }
+                "maximum" : "max_production_time",
+                "medium" : "bar",
+            },
+            "bar": 123
         }
